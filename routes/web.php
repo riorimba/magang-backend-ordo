91098b2dd67375b47a0e716c09b0d82535619228
riorimba <monhero78@gmail.co>
2024-12-04 16:23:18 +0700

    Routing

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+route::get('/hello', function(){
+    return 'Hello World';
+});
+
+route::get('/perkalian/{angka}', function($angka){
+    return $angka * 2;
+});
